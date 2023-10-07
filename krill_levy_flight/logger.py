e2030f3689c9a2206c3bf478a83917e527197001
gon-xeneta <98804721+gon-xeneta@users.noreply.github.com>
2023-10-08 01:50:49 +0200

    Add format to loggers

--- krill_levy_flight/logger.py
+++ krill_levy_flight/logger.py
@@ -3,5 +3,10 @@ import sys
 
 __all__ = ("logger",)
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="[%(asctime)s] [%(levelname)8s]:  %(message)s",
+)
+
 logger = logging.getLogger(__name__)
